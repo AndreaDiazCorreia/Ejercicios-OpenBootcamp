75f5a06610d65d0de0a35195cba65cd127652366
AndreaDiazCorreia <85651492+AndreaDiazCorreia@users.noreply.github.com>
2022-06-24 12:24:36 -0300

    Add files via upload

---
+++ EjercicioOpenbootcamp-1/src/SegundaParte/main.java
@@ -0,0 +1,8 @@
+package SegundaParte;
+
+public class main {
+    public static void main(String[] args){
+        coche carrito = new coche(4);
+        System.out.println(carrito.toString());
+    }
+}
